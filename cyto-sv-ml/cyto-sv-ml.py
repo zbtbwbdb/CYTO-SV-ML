d5bbcae95a8c2f41cb4f487f6e651cc090fd0f78
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-10-11 10:18:16 -0500

    Update cyto-sv-ml.py

--- cyto-sv-ml/cyto-sv-ml.py
+++ cyto-sv-ml/cyto-sv-ml.py
@@ -2,39 +2,31 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
 import seaborn as sns
 from pandas import read_csv
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
 from numpy import asarray
-from sklearn import svm, metrics, datasets
-from sklearn.metrics import median_absolute_error, roc_curve, auc, accuracy_score, precision_score, recall_score, precision_recall_curve, confusion_matrix
-from sklearn.datasets import fetch_openml
-from sklearn.model_selection import train_test_split
-from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, label_binarize
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
-from sklearn.preprocessing import StandardScaler
+from itertools import cycle
+from numpy import sqrt, argmax
 from itertools import *
-from sklearn.pipeline import make_pipeline
-#from sklearn.linear_model import Ridge
-#from sklearn.compose import TransformedTargetRegressor
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
-from sklearn import preprocessing
 import random
 import pickle
 from time import time
-from sklearn.utils import Bunch
-from sklearn.datasets import fetch_species_distributions
 import statistics
-from sklearn.multiclass import OneVsRestClassifier
-from itertools import cycle
-from matplotlib.pyplot import *
-from numpy import sqrt, argmax
-from sklearn.metrics import mean_squared_error
+from sklearn.utils import Bunch
 from supervised.automl import AutoML # mljar-supervised
 
+from sklearn import metrics, datasets
+from sklearn.metrics import mean_squared_error, median_absolute_error, roc_curve, auc, accuracy_score, precision_score, recall_score, precision_recall_curve, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_transformer
+from sklearn.pipeline import make_pipeline
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, label_binarize, LabelEncoder
+from sklearn.ensemble import GradientBoostingRegressor, GradientBoostingClassifier, AdaBoostClassifier
+
+
+# Input data
 sv=pd.read_csv("input_sv.vcf",sep="\t", header=0, index_col=None, keep_default_na=False)
 sv_type=sv['sv_type']
 
@@ -52,16 +44,20 @@ else:
 
 # data transformation
 sv_sel=sv.iloc[:,np.r_[np.where(sv.columns.isin(sv_columns.iloc[0,:]))]
-sv_tf=data_tf.transform(sv)
+X = sv_sel.drop("label", axis=1)               
+y = sv_sel.label.values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.5,stratify=X[['label']], random_state=0)
+X_train_tf=data_tf.transform(X_train)  
+X_test_tf=data_tf.transform(X_test)                 
                
 # AUTOML model fit with training data and validate with testing data              
-sv_train_model = cyto_sv_ml.fit(sv_tf_training)
-predictions_test = automl.predict_all(sv_tf_testing)
+sv_train_model = cyto_sv_ml.fit(X_train_tf)
+predictions_test = sv_train_model.predict_all(X_test_tf)
                
 # model performance metrics
-print("Test accuracy:", accuracy_score(y_testing, predictions_testing["label"].astype(int)))
-print(predictions_testing['label'].value_counts())
-print("Test micro precision_score:", precision_score(y_testing, predictions_testing["label"].astype(int),average='micro'))
-print("Test macro precision_score:", precision_score(y_testing, predictions_testing["label"].astype(int),average='macro'))
+print("Test accuracy:", accuracy_score(y_test, predictions_test["label"].astype(int)))
+print(predictions_test['label'].value_counts())
+print("Test micro precision_score:", precision_score(y_test, predictions_test["label"].astype(int),average='micro'))
+print("Test macro precision_score:", precision_score(y_test, predictions_test["label"].astype(int),average='macro'))
 print("Test micro recall_score:", recall_score(y_testing, predictions_testing["label"].astype(int),average='micro'))
 print("Test macro recall_score:", recall_score(y_testing, predictions_testing["label"].astype(int),average='macro'))
