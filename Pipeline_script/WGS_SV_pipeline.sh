0acfd11b856297ee25a3c287b665e9c763e1f669
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-10-14 11:36:59 -0500

    Update WGS_SV_pipeline.sh

--- Pipeline_script/WGS_SV_pipeline.sh
+++ Pipeline_script/WGS_SV_pipeline.sh
@@ -8,22 +8,32 @@ Komplexity: https://github.com/eclarke/komplexity
 #################################################################################################################
 #!/bin/bash
 
-# Parliment2 run
-sudo docker run --rm --privileged -v ${main_dir}/in/:/home/dnanexus/in -v ${main_dir}/out/:/home/dnanexus/out  docker.io/dongwonlee/parliament2-sing:v0.12 --bam ${sample}.bam --bai ${sample}.bam.bai -r hg38/hs38.fasta --fai  hg38/hs38.fasta.fai --prefix  ${sample} --filter_short_contigs  --breakdancer --breakseq --manta --cnvnator     --lumpy --delly_deletion --delly_insertion --delly_inversion --delly_duplication --genotype 
+# SV Parliment2 run
+sudo docker run --rm --privileged -v ${main_dir}/in/:/home/dnanexus/in -v ${main_dir}/out/:/home/dnanexus/out  docker.io/dongwonlee/parliament2-sing:v0.12 --bam ${sample}.bam --bai ${sample}.bam.bai -r hg38/hs38.fasta --fai  hg38/hs38.fasta.fai --prefix  ${sample} --filter_short_contigs --breakdancer --manta --cnvnator --delly_deletion --delly_insertion --delly_inversion --delly_duplication
 
-# ChromoSeq run
+# SV ChromoSeq run
 sudo docker run --rm --privileged  -v ${main_dir}/:/scratch  --entrypoint /bin/sh docker-basespace_chromoseq:latest -c '/usr/bin/java -Dconfig.file=/scratch/software/docker-${scratch}/${chromoseq_dir}/lsf/application.new.conf -jar /opt/cromwell-36.jar run -t wdl -i ${scratch}/${chromoseq_dir}/lsf/inputs.json.tmp ${scratch}/${chromoseq_dir}/workflow_files/Chromoseq.v17.wdl'
 
-# svtyper run
+# SV svtyper run
+for file in ${main_dir}/out/${sample}/${sample}.*.vcf 
+do
+svtyper --max_reads 100000 -i ${file} -B ${main_dir}/in/${sample}/${sample}.bam >  ${file}.svtyper
+done
 
-
-# Sequence complexity run
-
+# SV sequence complexity run
+for file in ${main_dir}/out/${sample}/${sample}.*.vcf 
+do
+svtyper --max_reads 100000 -i ${file} -B ${main_dir}/in/${sample}/${sample}.bam >  ${file}.svtyper
+done
 
 # SV calling consoldation
-SURVIVOR merge ${outdir}/${sample}/${sample}.list 1000 1 1 0 0 10  ${outdir}/${sample}/${sample}.SV.all 
+ls ${main_dir}/out/${sample}/${sample}.*.vcf.svtyper > ${main_dir}/out/${sample}/${sample}.list
+SURVIVOR merge ${main_dir}/out/${sample}/${sample}.list 1000 1 1 0 0 10  ${outdir}/${sample}/${sample}.sv.all 
+
 
 # SV database label
-python ../SVCNV_database_filter.py -i ${outdir}/${sample}/${sample}.SV.all.tf -t ${outdir}/SV_database/${SV_database} -d 1000 -p 0.5 -o ${outdir}/${sample}/${sample}.SV.all.tf_${SV_database} 
-python sv_bnd_info_mapping.py ${outdir}/${sample}/${sample}.SV.all.tf ${outdir}/SV_database/${SV_database} ${SV_database} ${nf} 
+python ../SVCNV_database_filter.py -i ${outdir}/${sample}/${sample}.sv.all.tf.nobnd -t ${outdir}/SV_database/${SV_database} -d 1000 -p 0.5 -o ${outdir}/${sample}/${sample}.sv.all.tf_${SV_database} 
+python sv_bnd_info_mapping.py ${outdir}/SV_database/${SV_database} ${outdir}/${sample}/${sample}.sv.all.tf.bnd  ${SV_database} ${nf} 
 
+# SV AutoML run
+python AutoML.py ${outdir}/${sample}/${sample}.sv.all.tf_all
