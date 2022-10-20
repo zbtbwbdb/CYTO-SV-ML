e8ff94775f1a753c5a21787a412e94b46bdb3802
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-10-20 13:32:24 -0500

    Update WGS_SV_pipeline.sh

--- Pipeline_script/WGS_SV_pipeline.sh
+++ Pipeline_script/WGS_SV_pipeline.sh
@@ -19,7 +19,7 @@ for sample in $(cat ${main_dir}/${sample_id_list})
         docker run --rm --privileged -v ${main_dir}/in/${sample}/:/home/dnanexus/in -v ${main_dir}/out/${sample}/:/home/dnanexus/out docker.io/dongwonlee/parliament2-sing:v0.12 --bam ${sample}.bam -r hg38/hs38.fasta --prefix ${sample} --filter_short_contigs --breakdancer --cnvnator --delly_deletion --delly_insertion --delly_inversion --delly_duplication
 
         # SV ChromoSeq run
-        docker run --rm --privileged  -v ${main_dir}/:/scratch  --entrypoint /bin/sh docker.io/zatawada/docker-basespace_chromoseq_v2:master -c '/usr/bin/java -Dconfig.file=/scratch/software/docker-${scratch}/${chromoseq_dir}/lsf/application.new.conf -jar /opt/cromwell-36.jar run -t wdl -i ${scratch}/${chromoseq_dir}/lsf/inputs.json.tmp ${scratch}/${chromoseq_dir}/workflow_files/Chromoseq.v17.wdl'
+        docker run --rm --privileged  -v ${main_dir}/:/scratch  --entrypoint /bin/sh zatawada/docker-basespace_chromoseq_v2:master -c '/usr/bin/java -Dconfig.file=/scratch/software/docker-${scratch}/${chromoseq_dir}/lsf/application.new.conf -jar /opt/cromwell-36.jar run -t wdl -i ${scratch}/${chromoseq_dir}/lsf/inputs.json.tmp ${scratch}/${chromoseq_dir}/workflow_files/Chromoseq.v17.wdl'
 
         # SV vcf preparation
         cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.*.vcf  ${main_dir}/out/${sample}/vcf_out/
