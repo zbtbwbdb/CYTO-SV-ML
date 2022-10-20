38d46710fd7f0ed983e921cc0a4a5ec11d76f22e
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-10-20 11:06:17 -0500

    Update sv_combine_all.py

--- Pipeline_script/sv_combine_all.py
+++ Pipeline_script/sv_combine_all.py
@@ -1,1 +1,20 @@
+import sys,os,re
+import pandas as pd
+import numpy as np
+from pathlib import Path
 
+# read the sv input and database vcf
+SV_database_name=str(sys.argv[2])
+info_vcf=pd.read_csv(str(sys.argv[1])+'svtyper.info',sep='\t',header=True,keep_default_na=False)  
+sc_vcf=pd.read_csv(str(sys.argv[1])+'.all_anno',sep='\t',header=None)    
+db_vcf=pd.read_csv(str(sys.argv[1])+'.bpst_bpend.fa.out.kz.index_complex',sep='\t',header=None) 
+out_vcf=pd.read_csv(str(sys.argv[1])+'.all_combine',sep='\t',header=None)
+
+# simplify the sv database vcf
+db_vcf_sim=db_vcf.iloc[:, np_r[1,(db_vcf.shape[1]-1):(db_vcf.shape[1]-1)]]
+
+# matching database label 
+all_combine_vcf=pd.merge([info_vcf, sc_vcf, db_vcf_sim])
+
+# export to csv
+all_combine_vcf.to_csv(out_vcf,sep='\t',index=False,header=True)
