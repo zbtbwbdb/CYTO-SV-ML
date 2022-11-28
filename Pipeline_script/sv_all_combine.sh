204cb579348c832d32e745263a4a99755a933392
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-11-28 17:54:43 -0600

    Update sv_all_combine.sh

--- Pipeline_script/sv_all_combine.sh
+++ Pipeline_script/sv_all_combine.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 main_dir=$1
-sample_vector=$2
+sample=$2
 
-for sample in $(echo ${sample_vector} | sed 's%@%\t%g')
-    do
-       echo ${sample}
-       echo "# combine the SV annotation, complexity, vcf and svtyper info" && date               
-       awk 'FNR==NR{a[$2];b[$2]=$0;next} ($1 in a) {print $0"\t"b[$1]}' ${main_dir}/out/${sample}/${sample}.10k.sv.all.sv_id_mapping.all_info ${main_dir}/out/${sample}/${sample}.10k.sv.all.sv_id_mapping.all_anno > ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info 
-       
-       awk 'FNR==NR{a[$6];b[$6]=$0;next} ($1 in a){print $0"\t"b[$1]}'  ${main_dir}/out/${sample}/${sample}.10k.sv.all.bed.bpst_bpend.kz.index_complex ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info >  ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex
-       awk 'FNR==NR{a[$3];b[$3]=$7;next} {if ($1 in a) {print $0"\t"b[$1]} else{print $0"\t1"}}'  ${main_dir}/out/${sample}/${sample}.10k.nontrs_tf.all.sv_info.sim   ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex > ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex.supp          
-     #  awk '{print NF}' ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex.supp | awk '!a[$1]++'         
-    done
+echo ${sample}
+echo "# combine the SV annotation, complexity, vcf and svtyper info" && date               
+awk 'FNR==NR{a[$2];b[$2]=$0;next} ($1 in a) {print $0"\t"b[$1]}' ${main_dir}/out/${sample}/${sample}.10k.sv.all.sv_id_mapping.all_info ${main_dir}/out/${sample}/${sample}.10k.sv.all.sv_id_mapping.all_anno > ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info 
+awk 'FNR==NR{a[$6];b[$6]=$0;next} ($1 in a){print $0"\t"b[$1]}'  ${main_dir}/out/${sample}/${sample}.10k.sv.all.bed.bpst_bpend.kz.index_complex ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info >  ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex
+awk 'FNR==NR{a[$3];b[$3]=$7;next} {if ($1 in a) {print $0"\t"b[$1]} else{print $0"\t1"}}'  ${main_dir}/out/${sample}/${sample}.10k.nontrs_tf.all.sv_info.sim   ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex > ${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex.supp          
