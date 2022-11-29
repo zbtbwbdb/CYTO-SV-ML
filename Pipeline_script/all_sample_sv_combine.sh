3e56b150af19a51c54341c81575a85f8c9fb4ed2
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-11-29 00:00:57 -0600

    Update all_sample_sv_combine.sh

--- Pipeline_script/all_sample_sv_combine.sh
+++ Pipeline_script/all_sample_sv_combine.sh
@@ -4,9 +4,18 @@ sample_all=$2
 SAMPLES_vector=$3
 
 echo "# combine all sample sv " && date
-for sample in $(echo ${SAMPLES_vector} | sed 's%@%%\t%g')
+sudo rm -rf ${main_dir}/out/${sample_all}.sv.all.combine_all
+sed "s%@%\n%g" ${SAMPLES_vector} > ${SAMPLES_vector}.tmp
+sm_ln=$(wc -l ${SAMPLES_vector}.tmp | awk '{print $1}')
+for i in $(seq 1 $sm_ln)
   do
-    cat ${sample}.sv_all_combine} >> ${main_dir}/out/${sample_all}.sv.all.combine_all.tmp 
+    sample=$(awk -v a="$i" '(FNR==a){print $1}' ${SAMPLES_vector}.tmp)
+    sample_file="${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex.supp"
+    echo ${sample} ${sample_file}
+    if (( i==1 )); then
+        awk -v a="$sample" '{if (FNR==1) {print $0"\tsample_id"} else {print $0"\t"a}}' ${sample_file} >> ${main_dir}/out/${sample_all}.sv.all.combine_all
+    else
+        awk -v a="$sample" '(FNR>1){print $0"\t"a}' ${sample_file} >> ${main_dir}/out/${sample_all}.sv.all.combine_all      
+        fi
   done
-awk '(FNR==1)||($1!~"sv_id"){print $0}' ${main_dir}/out/${sample_all}.sv.all.combine_all.tmp > ${main_dir}/out/${sample_all}.sv.all.combine_all
-rm -rf  ${main_dir}/out/${sample_all}.sv.all.combine_all.tmp 
+sudo rm -rf ${SAMPLES_vector}.tmp
