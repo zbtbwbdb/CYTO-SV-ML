9fe2da7962cb1ca8d0be33fc169a0dd49322c5c2
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 17:44:55 -0600

    Update svtyper_qc.sh

--- Pipeline_script/svtyper_qc.sh
+++ Pipeline_script/svtyper_qc.sh
@@ -15,9 +15,9 @@ for svtype in manta delly cnvnator breakdancer
 # SV svtyper run    
 echo "# run sytyper for all nontrs SV" && date
 svtyper-sso --core 8 --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all -B ${main_dir}/out/${sample}/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyper
-python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyper
+python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyped.vcf
 
 echo "# run sytyper for all trs SV" && date
 svtyper-sso --core 8 --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all -B ${main_dir}/out/${sample}/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyper
-python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyper 
+python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf
  rm -rf ${main_dir}/out/${sample}/${sample}.bam*
