d690a44621ba56f2aeb0cdb5ca00edd68034a1c4
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 15:11:33 -0600

    Update svtyper_qc.sh

--- Pipeline_script/svtyper_qc.sh
+++ Pipeline_script/svtyper_qc.sh
@@ -4,12 +4,13 @@ cyto_dv_ml_dir=$2
 sample=$3
 size_k=$4
 
-# for svtype in manta delly cnvnator breakdancer       
-#     do     
-#        echo ${svtype}              
-#        python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf 10000 down > ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k
-#        python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k  
-#     done
+for svtype in manta delly cnvnator breakdancer       
+    do     
+       echo ${svtype}              
+       svtyper-sso --core 8 --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k -B ${main_dir}/out/${sample}/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k      
+#       python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf 10000 down > ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k
+       python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k  
+    done
                
 # SV svtyper run    
 echo "# run sytyper for all nontrs SV" && date
