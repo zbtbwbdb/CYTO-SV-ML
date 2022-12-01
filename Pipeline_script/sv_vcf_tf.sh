5bc07ea72f20c70d211b226dd3788fe38e7fda38
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-11-30 20:59:14 -0600

    Update sv_vcf_tf.sh

--- Pipeline_script/sv_vcf_tf.sh
+++ Pipeline_script/sv_vcf_tf.sh
@@ -12,11 +12,11 @@ for svtype in  breakdancer cnvnator delly.deletion delly.duplication delly.inver
         python ${cyto_dv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf f
         mv ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.re_id ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf    
         echo ${svtype} "SV size tf" && date     
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.10k
+        python ${cyto_dv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
          echo ${svtype} "SV type tf" && date                
-         python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.10k
+         python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
         echo ${svtype} "SV info tf" && date                 
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.10k
+        python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
     done
 
-cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.10k.trs_tf ${main_dir}/out/${sample}/${sample}.10k.trs_tf.all
+cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.${size}.trs_tf ${main_dir}/out/${sample}/${sample}.${size}.trs_tf.all
