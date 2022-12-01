ccb9309e41f9abbff1a62fe4f18541b11f6c884e
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-11-30 21:01:34 -0600

    Update sv_vcf_tf.sh

--- Pipeline_script/sv_vcf_tf.sh
+++ Pipeline_script/sv_vcf_tf.sh
@@ -3,6 +3,7 @@ main_dir=$1
 cyto_dv_ml_dir=$2
 sample=$3
 size=$4
+size_k=$((size/1000))
 
 echo "# prepare the SV vcf files with size restriction and extract sv_vcf_info" && date 
 
@@ -12,11 +13,11 @@ for svtype in  breakdancer cnvnator delly.deletion delly.duplication delly.inver
         python ${cyto_dv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf f
         mv ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.re_id ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf    
         echo ${svtype} "SV size tf" && date     
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
+        python ${cyto_dv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
          echo ${svtype} "SV type tf" && date                
-         python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
+         python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
         echo ${svtype} "SV info tf" && date                 
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size}
+        python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
     done
 
-cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.${size}.trs_tf ${main_dir}/out/${sample}/${sample}.${size}.trs_tf.all
+cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.${size_k}.trs_tf ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all
