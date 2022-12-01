be73315d3af13e313859a831c935654348224734
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-11-30 20:30:48 -0600

    Update svmerge_qc.sh

--- Pipeline_script/svmerge_qc.sh
+++ Pipeline_script/svmerge_qc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 main_dir=$1
-sample=$2
+cyto_dv_ml_dir=$2
+sample=$3
 
 echo "# merge all nontrs SV" && date
 ls ${main_dir}/out/${sample}/sv_caller_results/${sample}.*.vcf.10k.nontrs_tf | grep -v svtyper > ${main_dir}/out/${sample}/sv_caller_results/${sample}.10k.nontrs_tf.list
@@ -11,8 +12,8 @@ ls ${main_dir}/out/${sample}/sv_caller_results/${sample}.*.vcf.10k.*trs_tf | gre
 SURVIVOR merge ${main_dir}/out/${sample}/sv_caller_results/${sample}.10k.all.list 1000 1 1 0 0 10  ${main_dir}/out/${sample}/${sample}.10k.sv.all 
 
 echo "# SV caller SUPP info extraction" && date
-python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py  ${main_dir}/out/${sample}/${sample}.10k.nontrs_tf.all SUPP
-python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_consolidate_id_mapping.py ${main_dir}/out/${sample}/${sample}.10k.sv.all 
+python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py  ${main_dir}/out/${sample}/${sample}.10k.nontrs_tf.all SUPP
+python ${cyto_dv_ml_dir}/Pipeline_script/sv_consolidate_id_mapping.py ${main_dir}/out/${sample}/${sample}.10k.sv.all 
 
 echo "# SV vcf simplified transformation" && date
-python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_vcf_sim.py ${main_dir}/out/${sample}/${sample}.10k.sv.all
+python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_sim.py ${main_dir}/out/${sample}/${sample}.10k.sv.all
