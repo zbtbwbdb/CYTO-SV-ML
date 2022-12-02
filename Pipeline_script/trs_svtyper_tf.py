fe41eb7e00884bd604b26504d24f8dac08881bf9
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-02 15:44:15 -0600

    Create trs_svtyper_tf.py

---
+++ Pipeline_script/trs_svtyper_tf.py
@@ -0,0 +1,36 @@
+import sys,os,re, collections
+in_vcf=open(sys.argv[1],'r')
+out_vcf=open(str(sys.argv[1])+'.sv_info','w')
+
+def trs_svtyper_tf(line):
+    info_dict={}
+    item=line.strip().split('\t')
+    
+    # info column extraction
+    info=item[7]
+    info_list=info.split(';') 
+    for inf in info_list:
+        if '=' in inf:
+            info_dict[inf.split('=')[0]]=inf.split('=')[1]
+        else:
+            info_dict[inf]=inf
+    
+    # correct mate_id
+    mate_id=info_dict['MATEID']   
+    mate_id_key=mate_id.split(':')[-1]
+    mate_id_cr=mate_id.split(':')[0:-1]      
+    if mate_id_key==0:
+      item[2]=mate_id_cr+':1'
+    elif mate_id_key==1:
+      item[2]=mate_id_cr+':0' 
+    else:
+      print "error!!!"
+    line='\t'.join(str(l) for l in item)+'\t'
+    return line
+  
+for line in in_vcf:
+    if line.startswith('#'):
+        out_vcf.write(line)             
+    else:
+        out_vcf.write(trs_svtyper_tf(line))   
+        n=1
