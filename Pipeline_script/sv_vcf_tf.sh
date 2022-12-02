cf47c3a002e7569b3317f38a6a9cfb8260bb3586
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 21:09:51 -0600

    Update sv_vcf_tf.sh

--- Pipeline_script/sv_vcf_tf.sh
+++ Pipeline_script/sv_vcf_tf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 main_dir=$1
-cyto_dv_ml_dir=$2
+cyto_sv_ml_dir=$2
 sample=$3
 size=$4
 size_k=$((size/1000))
@@ -10,14 +10,14 @@ echo "# prepare the SV vcf files with size restriction and extract sv_vcf_info" 
 # SV vcf transformation
 for svtype in  breakdancer cnvnator delly.deletion delly.duplication delly.inversion ichnorcnv manta       
     do
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf f
+        python ${cyto_sv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf f
         mv ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.re_id ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf    
         echo ${svtype} "SV size tf" && date     
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
+        python ${cyto_sv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
          echo ${svtype} "SV type tf" && date                
-         python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
+         python ${cyto_sv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
         echo ${svtype} "SV info tf" && date                 
-        python ${cyto_dv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
+        python ${cyto_sv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k
     done
 
 cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.${size_k}k.trs_tf ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all
