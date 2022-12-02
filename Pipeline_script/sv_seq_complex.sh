caca7779ea195e25a304bb8155fbea885c43c54d
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 21:11:07 -0600

    Update sv_seq_complex.sh

--- Pipeline_script/sv_seq_complex.sh
+++ Pipeline_script/sv_seq_complex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 main_dir=$1
-cyto_dv_ml_dir=$2
+cyto_sv_ml_dir=$2
 sample=$3
 py27_dir=$4
 size_k=$5
@@ -8,18 +8,18 @@ size_k=$5
 # SV sequence complexity run 
 
 echo "# extract SV coordinate information" && date
-python ${cyto_dv_ml_dir}/Pipeline_script/sv_vcf_bed_tf.py ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all
-awk 'FNR==NR{a[$1];b[$1]=$2;next}{c=b[$1]-150 ; if (($2>=150)&&($2<=c)) {$2=$2-150; $3=$2+150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else if ($2>c) {$2=c-150;$3=c+150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else if ($2<150){$2=1;$3=300; print $1"\t"$2"\t"$3"\t"$5"\t"$6}}' ${cyto_dv_ml_dir}/reference/hg38_chromosome_size.txt ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed | sed 's% %\t%g' > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.bpst
-awk 'FNR==NR{a[$1];b[$1]=$2;next}{$1=$4; $4=$1; c=b[$1]-150 ; if ($3>=c) {$3=c+150;$2=c-150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else {$2=$3-150; $3=$3+150;  print $1"\t"$2"\t"$3"\t"$5"\t"$6}}' ${cyto_dv_ml_dir}/reference/hg38_chromosome_size.txt ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed | sed 's% %\t%g' > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.bpend
+python ${cyto_sv_ml_dir}/Pipeline_script/sv_vcf_bed_tf.py ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all
+awk 'FNR==NR{a[$1];b[$1]=$2;next}{c=b[$1]-150 ; if (($2>=150)&&($2<=c)) {$2=$2-150; $3=$2+150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else if ($2>c) {$2=c-150;$3=c+150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else if ($2<150){$2=1;$3=300; print $1"\t"$2"\t"$3"\t"$5"\t"$6}}' ${cyto_sv_ml_dir}/reference/hg38_chromosome_size.txt ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed | sed 's% %\t%g' > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.bpst
+awk 'FNR==NR{a[$1];b[$1]=$2;next}{$1=$4; $4=$1; c=b[$1]-150 ; if ($3>=c) {$3=c+150;$2=c-150; print $1"\t"$2"\t"$3"\t"$5"\t"$6} else {$2=$3-150; $3=$3+150;  print $1"\t"$2"\t"$3"\t"$5"\t"$6}}' ${cyto_sv_ml_dir}/reference/hg38_chromosome_size.txt ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed | sed 's% %\t%g' > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.bpend
 
 
 export PATH=${main_dir}/software/SeqComplex:$PATH
-cd ${cyto_dv_ml_dir}/software/SeqComplex
+cd ${cyto_sv_ml_dir}/software/SeqComplex
 for bp in bpst bpend
     do
        echo ${bp}
        echo "# make bed file for SV breakpoints" && date       
-       ${py27_dir}/bedtools getfasta -fi ${cyto_dv_ml_dir}/reference/hg38/hs38.fasta -bed ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp} -fo ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out
+       ${py27_dir}/bedtools getfasta -fi ${cyto_sv_ml_dir}/reference/hg38/hs38.fasta -bed ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp} -fo ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out
       
        # remove lowcomplex line with "NNNN"+ 1 "A/T/C/G"
        grep NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out -n > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.cr
@@ -35,7 +35,7 @@ for bp in bpst bpend
       # rm ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.tmp   
       
        echo "# run SeqComplex and KZ for SV breakpoints" && date
-       perl ${cyto_dv_ml_dir}/software/SeqComplex/profileComplexSeq.pl ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.lc
+       perl ${cyto_sv_ml_dir}/software/SeqComplex/profileComplexSeq.pl ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.lc
        kz --fasta < ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.kz
        awk 'FNR==NR{a[$1]; b[$1]=$0; next}{c=$1":"$2"-"$3; if (c in a) {print $0"\t"b[c]}}' ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.kz ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp} > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.kz.index
        awk 'FNR==NR{a[$1]; b[$1]=$0; next}($6 in a) {print $0"\t"b[$6]}' ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.complex ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.kz.index > ${main_dir}/out/${sample}/${sample}.${size_k}k.sv.all.bed.${bp}.fa.out.kz.index_complex
