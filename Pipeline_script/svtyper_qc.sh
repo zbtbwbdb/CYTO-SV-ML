25640daff6cdf42491ce9b4df8fce59b3dc03597
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 22:01:28 -0600

    Update svtyper_qc.sh

--- Pipeline_script/svtyper_qc.sh
+++ Pipeline_script/svtyper_qc.sh
@@ -5,18 +5,18 @@ sample=$3
 sv_caller=$4
 size_k=$5
 
-sv_caller_vector= $(echo ${sv_caller} | sed "s%@%\n%g") 
-echo $sv_caller_vector
-sc_ln= $(echo sv_caller_vector | awk '(FNR==1){print NF}')
+echo ${sv_caller} | sed "s%@%\n%g" > sv_caller_vector.tmp
+sc_ln=$(wc -l sv_caller_vector.tmp | awk '{print $1}')
 echo "# run sytyper for all sv callers" && date
 for i in $(seq 1 $sc_ln)
    do
-       svtype=$(echo ${sv_caller_vector} | awk -v a="$i" '(FNR==1){print $a}')  
+       svtype=$(awk -v a="$i" '(FNR==a){print $1}' sv_caller_vector.tmp)  
        echo ${svtype}              
        svtyper-sso --core 8 --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${svtype}.vcf.${size_k}k -B ${main_dir}/out/${sample}/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k      
        python ${main_dir}/software/CYTO-SV-ML/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/${sample}.${svtype}.svtyped.vcf.${size_k}k  
     done
-               
+sudo rm -rf sv_caller_vector.tmp 
+
 # SV svtyper run    
 echo "# run sytyper for all nontrs SV" && date
 svtyper-sso --core 8 --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all -B ${main_dir}/out/${sample}/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyped.vcf
