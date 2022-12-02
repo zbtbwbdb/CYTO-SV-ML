35328a68c9eef4cbdd72325bceda9d261e125322
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-02 10:30:54 -0600

    Update sv_vcf_tf.sh

--- Pipeline_script/sv_vcf_tf.sh
+++ Pipeline_script/sv_vcf_tf.sh
@@ -10,12 +10,13 @@ echo "# prepare the SV vcf files with size restriction and extract sv_vcf_info" 
 # SV vcf transformation
 for sv_caller in  breakdancer cnvnator delly.deletion delly.duplication delly.inversion ichnorcnv manta       
     do
+        echo ${sv_caller}
         python ${cyto_sv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf f
         mv ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.re_id ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf    
         echo ${sv_caller} "SV size tf" && date     
         python ${cyto_sv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k
-         echo ${sv_caller} "SV type tf" && date                
-         python ${cyto_sv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k
+        echo ${sv_caller} "SV type tf" && date                
+        python ${cyto_sv_ml_dir}/Pipeline_script/sv_vcf_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k
         echo ${sv_caller} "SV info tf" && date                 
         python ${cyto_sv_ml_dir}/Pipeline_script/sv_info_tf_sim.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k
     done
