de9b67b0bd7422f71a1e3093ba46def256d0977c
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-01 21:41:17 -0600

    Update svmerge_qc.sh

--- Pipeline_script/svmerge_qc.sh
+++ Pipeline_script/svmerge_qc.sh
@@ -4,9 +4,10 @@ cyto_sv_ml_dir=$2
 sample=$3
 size_k=$4
 
-echo "# merge all nontrs SV" && date
+echo "# merge all non/trs SV" && date
 ls ${main_dir}/out/${sample}/sv_caller_results/${sample}.*.vcf.${size_k}k.nontrs_tf | grep -v svtyper > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${size_k}k.nontrs_tf.list
 SURVIVOR merge ${main_dir}/out/${sample}/sv_caller_results/${sample}.${size_k}k.nontrs_tf.list 1000 1 1 0 0 10  ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all
+cp ${main_dir}/out/${sample}/sv_caller_results/${sample}.manta.vcf.${size_k}k.trs_tf ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all
 
 echo "# merge all SV" && date
 ls ${main_dir}/out/${sample}/sv_caller_results/${sample}.*.vcf.${size_k}k.*trs_tf | grep -v svtyper > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${size_k}k.all.list
