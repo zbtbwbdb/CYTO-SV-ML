e988917f939ab9d73d6ed3542d6203dbe0bdd4d5
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-02 22:11:24 -0600

    Update all_sample_sv_combine.sh

--- Pipeline_script/all_sample_sv_combine.sh
+++ Pipeline_script/all_sample_sv_combine.sh
@@ -9,15 +9,17 @@ sudo mkdir ${main_dir}/out/${sample_all}/
 sudo chmod 777 -R ${main_dir}/out/${sample_all}/
 sed "s%@%\n%g" ${SAMPLES_vector} > ${SAMPLES_vector}.tmp
 sm_ln=$(wc -l ${SAMPLES_vector}.tmp | awk '{print $1}')
+
 for i in $(seq 1 $sm_ln)
   do
     sample=$(awk -v a="$i" '(FNR==a){print $1}' ${SAMPLES_vector}.tmp)
     sample_file="${main_dir}/out/${sample}/${sample}.10k.sv.all.all_anno.all_info.all_complex.supp"
     echo ${sample} ${sample_file}
     if (( i==1 )); then
-        awk -v a="$sample" '{if (FNR==1) {print $0"\tsample_id"} else {print $0"\t"a}}' ${sample_file} >> ${main_dir}/out/${sample_all}/${sample_all}.sv.all.combine_all
+        awk -v a="$sample" '{if (FNR==1) {print $0"\tsample_id"} else {print $0"\t"a}}' ${sample_file} > ${main_dir}/out/${sample_all}/${sample_all}.sv.all.combine_all
     else
         awk -v a="$sample" '(FNR>1){print $0"\t"a}' ${sample_file} >> ${main_dir}/out/${sample_all}/${sample_all}.sv.all.combine_all      
         fi
   done
 sudo rm -rf ${SAMPLES_vector}.tmp
+
