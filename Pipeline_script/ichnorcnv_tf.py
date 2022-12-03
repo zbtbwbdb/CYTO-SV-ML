ae2dbea493c5c34a9ba64f1f4a4edac6c1cd3749
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-03 09:01:26 -0600

    Add files via upload

--- Pipeline_script/ichnorcnv_tf.py
+++ Pipeline_script/ichnorcnv_tf.py
@@ -7,7 +7,7 @@ def ichnorcnv_tf(line):
     sv_dict={}
     item=line.strip().split('\t')
     if item[0].startswith('ID'):
-        line='#CHROM\tPOS\tID\tREF\ALT\QUAL\tFILTER\tINFO\tFORMAT\t'+sm_id+'\n'
+        line='#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t'+sm_id+'\n'
         return line
     else:                  
         genotype='0/1'  
@@ -35,5 +35,5 @@ def ichnorcnv_tf(line):
 out_vcf.write('##fileformat=VCFv4.1\n##fileDate=20221013\n##reference=1000GenomesPhase3_decoy-GRCh37\n##source=CNVnator\n##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the variant described in this record">\n##INFO=<ID=IMPRECISE,Number=0,Type=Flag,Description="Imprecise structural variation">\n##INFO=<ID=SVLEN,Number=1,Type=Integer,Description="Difference in length between REF and ALT alleles">\n##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">\n##INFO=<ID=natorRD,Number=1,Type=Float,Description="Normalized RD">\n##INFO=<ID=natorP1,Number=1,Type=Float,Description="e-val by t-test">\n##INFO=<ID=natorP2,Number=1,Type=Float,Description="e-val by Gaussian tail">\n##INFO=<ID=natorP3,Number=1,Type=Float,Description="e-val by t-test (middle)">\n##INFO=<ID=natorP4,Number=1,Type=Float,Description="e-val by Gaussian tail (middle)">\n##INFO=<ID=natorQ0,Number=1,Type=Float,Description="Fraction of reads with 0 mapping quality">\n##INFO=<ID=natorPE,Number=1,Type=Integer,Description="Number of paired-ends support the event">\n##INFO=<ID=SAMPLES,Number=.,Type=String,Description="Sample genotyped to have the variant">\n##ALT=<ID=DEL,Description="Deletion">\n##ALT=<ID=DUP,Description="Duplication">\n##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">\n##FORMAT=<ID=CN,Number=1,Type=Integer,Description="Copy number genotype for imprecise events">\n##FORMAT=<ID=PE,Number=1,Type=Integer,Description="Number of paired-ends that support the event">\n') 
 for line in in_vcf:    
     item=line.strip().split('\t')
-    if item[11]!='NEUT' and item[11]!='Corrected_Call':                         
+    if item[11]!='NEUT':                         
     	out_vcf.write(ichnorcnv_tf(line))
