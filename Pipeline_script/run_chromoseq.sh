827a75cac49d3cb684f872e42e7ffcd442638f70
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-02 22:14:26 -0600

    Update run_chromoseq.sh

--- Pipeline_script/run_chromoseq.sh
+++ Pipeline_script/run_chromoseq.sh
@@ -15,8 +15,8 @@ sudo docker run --rm --privileged  -v ${main_dir}/:/scratch   -v ${cyto_sv_ml_di
 
 echo "# prepare the SV db uwstl_s file for chromoseq pipeline" && date 
 awk '($1=="DUP")||($1=="DEL")||($1=="BND")||($1=="INV"){print $2"\t"$3"\t"$5"\t"$1"\t"$2":"$3":"$5":"$1}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.chromoseq.txt > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs
-sort -k 1,1 -k 2,3n ${main_dir}/out/${sample}/sv_caller_results${sample}.uwstl_s.nontrs | bgzip -f > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs.gz
-tabix -p vcf ${main_dir}/out/${sample}/sv_caller_results${sample}.uwstl_s.nontrs.gz
+sort -k 1,1 -k 2,3n ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs | bgzip -f > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs.gz
+tabix -p vcf ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs.gz
 awk '($1=="BND"){print $2"\t"$3"\t"$5"\t"$4"\t"$2":"$3":"$5":"$1}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.chromoseq.txt > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.trs   
          
 echo "# prepare the SV vcf files (manta + ichnorcnv)" && date 
