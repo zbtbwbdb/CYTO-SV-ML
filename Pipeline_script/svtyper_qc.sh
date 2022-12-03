0135ba12003e57af9e83590fa5e08bc303cae040
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-02 21:30:04 -0600

    Update svtyper_qc.sh

--- Pipeline_script/svtyper_qc.sh
+++ Pipeline_script/svtyper_qc.sh
@@ -4,7 +4,7 @@ cyto_sv_ml_dir=$2
 sample=$3
 sv_caller_vector=$4
 size_k=$5
-# svtyper="svtyper-sso --core 8"
+svtyper="svtyper-sso --core 8"
 
 echo ${sv_caller_vector} | sed "s%@%\n%g" > sv_caller_vector.tmp
 sc_ln=$(wc -l sv_caller_vector.tmp | awk '{print $1}')
@@ -14,8 +14,8 @@ for i in $(seq 1 $sc_ln)
        sv_caller=$(awk -v a="$i" '(FNR==a){print $1}' sv_caller_vector.tmp)  
        echo ${sv_caller}   
        python ${cyto_sv_ml_dir}/Pipeline_script/trs_svtyper_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k.trs_tf 
-       svtyper --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k.trs_tf.tmp -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.trs_tf.svtyped.vcf      
-       svtyper --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k.nontrs_tf -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.nontrs_tf.svtyped.vcf     
+       $(svtyper} --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k.trs_tf.tmp -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.trs_tf.svtyped.vcf      
+       $(svtyper} --max_reads 100000 -i ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k.nontrs_tf -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.nontrs_tf.svtyped.vcf     
        if [ -s ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.trs_tf.svtyped.vcf ]; then       
             awk '($1!~"#"){print $0}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.trs_tf.svtyped.vcf > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.${size_k}k.trs_tf.svtyped.vcf.tmp           
        fi
@@ -31,11 +31,11 @@ sudo rm -rf sv_caller_vector.tmp
 
 # SV svtyper run    
 echo "# run sytyper for all nontrs SV" && date
-svtyper --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyped.vcf
+$(svtyper} --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyped.vcf
 
 echo "# run sytyper for all trs SV" && date
 python ${cyto_sv_ml_dir}/Pipeline_script/trs_svtyper_tf.py ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all
-svtyper --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.tmp -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf
+$(svtyper} --max_reads 100000 -i ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.tmp -B ${main_dir}/in/${sample}.bam > ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf
 
 awk '($1!~"#"){print $0}' ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf > ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf.tmp 
 cat ${main_dir}/out/${sample}/${sample}.${size_k}k.nontrs_tf.all.svtyped.vcf ${main_dir}/out/${sample}/${sample}.${size_k}k.trs_tf.all.svtyped.vcf.tmp > ${main_dir}/out/${sample}/${sample}.${size_k}k.all.svtyped.vcf
