cf2c1220be1cf84a34f6e3a6ea44f481ea8451ed
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-03 12:12:05 -0600

    Update run_parliment2.sh

--- Pipeline_script/run_parliment2.sh
+++ Pipeline_script/run_parliment2.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 main_dir=$1
-parliment_docker=$2
-sample=$3  
+cyto_sv_ml_dir=$2
+parliment_docker=$3
+sample=$4  
 
 echo "# run parliament docker" && date
 mkdir ${main_dir}/in/reference/
 cp ${main_dir}/in/${sample}.bam ${main_dir}/in/input.bam
 cp ${main_dir}/in/${sample}.bam.bai ${main_dir}/in/input.bam.bai
-cp ${main_dir}/reference/hg38/hs38.fasta ${main_dir}/in/reference/hg38/hs38.fasta
-cp ${main_dir}/reference/hg38/hs38.fasta.fai ${main_dir}/in/reference/hg38/hs38.fasta.fai
+cp ${cyto_sv_ml_dir}/reference/hg38/hs38.fasta ${main_dir}/in/reference/hg38/hs38.fasta
+cp ${cyto_sv_ml_dir}/reference/hg38/hs38.fasta.fai ${main_dir}/in/reference/hg38/hs38.fasta.fai
 sudo docker run --rm --privileged -v ${main_dir}/in/:/home/dnanexus/in -v ${main_dir}/out/${sample}/:/home/dnanexus/out ${parliment_docker} --bam ${sample}.bam  --bai ${sample}.bam.bai -r reference/hg38/hs38.fasta --fai reference/hg38/hs38.fasta.fai --prefix ${sample} --filter_short_contigs --breakdancer --cnvnator --delly_deletion --delly_inversion --delly_duplication
 rm -rf ${main_dir}/in/input.bam*
 rm -rf ${main_dir}/in/reference/
