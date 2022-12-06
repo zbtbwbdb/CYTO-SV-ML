b10e62c07895353cf26c46cf7654d5260e61443c
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-05 21:46:08 -0600

    Update sv_vcf_sim.py

--- Pipeline_script/sv_vcf_sim.py
+++ Pipeline_script/sv_vcf_sim.py
@@ -15,7 +15,11 @@ import sys,os,re
 in_vcf=open(sys.argv[1],'r')
 trs_out_vcf=open(str(sys.argv[1])+'.trs','w')
 non_trs_out_vcf=open(str(sys.argv[1])+'.nontrs','w')
-
+try:
+    id_mode=str(sys.argv[2])
+except:
+    id_mode='a'
+    
 def trs_sv_sim(line):
     sv_dict={}
     item=line.strip().split('\t')
@@ -28,10 +32,10 @@ def trs_sv_sim(line):
           sv_bp_end=inf.split('=')[1]
         if inf.startswith('CHR2'):
           sv_chr2=inf.split('=')[1] 
-    if 'chr' not in item[2]:
+    if 'chr' not in item[2] and id_mode!='s':
         line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+str(sv_chr2)+'\t'+sv_type+'\t'+str(item[0])+':'+str(item[1])+':'+str(sv_bp_end)+':'+str(sv_chr2)+':BND:'+str(item[2])+'\n'
     else:
-            line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+str(sv_chr2)+'\t'+sv_type+'\t'+str(item[0])+':'+str(item[1])+':'+str(sv_bp_end)+':'+str(sv_chr2)+':BND'+'\n'
+            line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+str(sv_chr2)+'\t'+sv_type+'\t'+item[2]+'\n'
     return line  
   
 def nontrs_sv_sim(line):
@@ -44,10 +48,10 @@ def nontrs_sv_sim(line):
           sv_type=inf.split('=')[1]          
         if inf.startswith('END'):
           sv_bp_end=inf.split('=')[1]
-    if 'chr' not in item[2]:        
+    if 'chr' not in item[2] and id_mode!='s':       
         line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+sv_type+'\t'+str(item[0])+':'+str(item[1])+':'+str(sv_bp_end)+':'+str(item[0])+':'+str(sv_type)+':'+str(item[2])+'\n'
     else:
-        line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+sv_type+'\t'+str(item[0])+':'+str(item[1])+':'+str(sv_bp_end)+':'+str(item[0])+':'+str(sv_type)+'\n'     
+            line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+sv_type+'\t'+item[2]+'\n'
     return line  
   
 for line in in_vcf:
@@ -57,9 +61,7 @@ for line in in_vcf:
     elif item[0].startswith('#CHROM'):      
         trs_out_vcf.write('sv_chr\tsv_start_bp\tsv_end_bp\tsv_chr2\tsv_type\tsv_id\n')
         non_trs_out_vcf.write('sv_chr\tsv_start_bp\tsv_end_bp\tsv_type\tsv_id\n')
-    elif 'SVTYPE=TRA' in item[7] or 'MantaBND' in item[7]:
+    elif 'SVTYPE=TRA' in item[7] or 'SVTYPE=BND' in item[7] or 'MantaBND' in item[7]:
         trs_out_vcf.write(trs_sv_sim(line))        
     else:
-        if item[1]=="100500000":
-            print(nontrs_sv_sim(line))
         non_trs_out_vcf.write(nontrs_sv_sim(line))       
