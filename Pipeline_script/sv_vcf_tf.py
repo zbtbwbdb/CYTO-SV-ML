15bb5b3207a79f51d08f3df8b7787ac5078f64d0
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-06 22:14:20 -0600

    Update sv_vcf_tf.py

--- Pipeline_script/sv_vcf_tf.py
+++ Pipeline_script/sv_vcf_tf.py
@@ -1,104 +1,97 @@
 import sys,os,re
 in_vcf=open(sys.argv[1],'r')
-trs_out_vcf=open(str(sys.argv[1])+'.trs_tf','w')
-nontrs_out_vcf=open(str(sys.argv[1])+'.nontrs_tf','w')
+out_vcf=open(str(sys.argv[1])+'.re_id','w')
+try:
+    id_mode=str(sys.argv[2])
+except:
+    id_mode='a'
 
-def vcf_bnd_tf(line):
-    info_dict={}
-    info_list2=[] 
+def sv_id_tf(line):
     item=line.strip().split('\t')
-
-    # add CI info
-    info=item[7]    
-    if not bool(re.search('CIPOS', info)):
-        info=str(info)+";CIPOS=-160,160"
-    if not bool(re.search('CIEND', info)):  
-        info=str(info)+";CIEND=-160,160"  
-        
-    #info column extraction        
-    info_list=info.split(';')             
-    i=0
+    sv_chr2=item[0]
+    sv_end=item[1]
+    sv_type=re.sub('<|>','',item[4])
+    
+    #collect info from column extraction
+    info=item[7]
+    info_list=info.split(';') 
+    info_dict={}    
     for inf in info_list:
         if '=' in inf:
             info_dict[inf.split('=')[0]]=inf.split('=')[1]
         else:
-            info_list2.append(str(inf))
-            i=1
-    info_dict.update({'SVTYPE':'BND'})
-    #del info_dict["CSQ"]              
-    info_dict.pop("CSQ",None)
-    
-    # geno column extraction
-    geno_list=item[9]
-    geno=geno_list.split(':')      
-    geno_index=item[8].split(':') 
-#    print(geno_index)
-    gt=geno[geno_index.index('GT')]
-    pr=geno[geno_index.index('PR')]
-    rd_pr=geno[geno_index.index('PR')].split(',')[1]
-    sr="0,0"
-    rd_sp=0
-    if 'SR' in item[8]:
-        sr=geno[geno_index.index('SR')]
-        rd_sp=geno[geno_index.index('SR')].split(',')[1] 
-    su=int(rd_pr)+int(rd_sp)
-    item[8]="GT:PR:SU:PE:SR"
-    item[9]=geno[0]+":"+str(pr)+":"+str(su)+":"+str(rd_pr)+":"+str(sr)
-    
-    # modify info dict and list
-    info_dict.update({'SU':su,'PE':rd_pr,'SP':rd_sp})
-    info_list2=[]
-    for key,value in info_dict.items():
-        info_list2.append(str(key)+'='+str(value))
-    item[7]=';'.join(str(inf) for inf in info_list2)  
+            info_dict[inf]=inf+"&no&id"       
 
-    # print out line   
-    line='\t'.join(str(itm) for itm in item)+'\n'
-    return line
-
-def vcf_nonbnd_tf(line):
-    info_dict={}
-    info_list2=[]   
-    item=line.strip().split('\t')
-    if item[0].startswith('#'):
-        return line
-    else:
-        info=item[7]
-        
-        # add CI info
-        if not bool(re.search('CIPOS', info)):
-            info=str(info)+";CIPOS=-160,160"
-        if not bool(re.search('CIEND', info)):  
-            info=str(info)+";CIEND=-160,160"  
-        
-        # remove CSQ annotation
-        info_list=info.split(';') 
-        i=0
-        for inf in info_list:
-            if '=' in inf:
-                info_dict[inf.split('=')[0]]=inf.split('=')[1]
-            else:
-                info_list2.append(str(inf))
-                i=1
-        info_dict.pop("CSQ",None)                        
+    # correct sv info        
+    if 'SVTYPE=' not in info_list:
+        if 'DEL|DUP|INV|INS|TRA|BND' in item[2]:
+            info_dict['SVTYPE']=re.findall('DEL|DUP|INV|INS|TRA|BND',item[2])[0]
+        elif 'DEL|DUP|INV|INS|TRA|BND' in item[4]:
+            info_dict['SVTYPE']=re.findall('DEL|DUP|INV|INS|TRA|BND',item[4])[0]
             
-        # modify info dict and list
-        for key,value in info_dict.items():
-            info_list2.append(str(key)+'='+str(value))
-        item[7]=';'.join(str(inf) for inf in info_list2)  
-        
-        line='\t'.join(str(itm) for itm in item)+'\n'
-        return line    
+    alt_info=re.sub('\[|\]',':',item[4])     
+    if 'CHR2=' not in info_list:
+        if info_dict['SVTYPE']=='BND' or info_dict['SVTYPE']=='TRA':
+            for ai in alt_info.split(':'):
+                if re.findall('chr',ai):
+                    info_dict['CHR2']=ai
+        else:
+            info_dict['CHR2']=item[0]   
+            
+    if 'END=' not in info:
+        if info_dict['SVTYPE']=='BND' or info_dict['SVTYPE']=='TRA':
+            for ai in alt_info.split(':'):
+                if ai.isdigit():
+                    info_dict['END']=ai
+        else:
+            info_dict['END']=item[1]
+            
+    sv_end=info_dict['END']   
+    sv_chr2=info_dict['CHR2']
+    sv_type=info_dict['SVTYPE']  
+ #   print(str(item[1])+":"+str(sv_end))        
+    # fix sv_id column extraction
+    if id_mode=='f' or id_mode=='sf':
+        sv_id=item[0]+':'+item[1] +':'+sv_end+':'+sv_chr2 +':'+sv_type+":"+item[2]
+    elif (id_mode=='c' or  id_mode=='sc') and 'chr' not in item[2]:
+        sv_id=item[0]+':'+item[1] +':'+sv_end+':'+sv_chr2 +':'+sv_type+":"+item[2]        
+    elif 'chr' in item[2] or re.findall('DEL|DUP|INV|INS|TRA|BND|CNV',item[2]) or re.findall('del|dup|inv|ins|tra|bnd|cnv',item[2]) or ':' in item[2]:
+        sv_id=item[2]
+    else:
+        sv_id=item[0]+':'+item[1] +':'+sv_end+':'+sv_chr2 +':'+sv_type+":"+item[2]
+    item[2]=sv_id
+    
+    # re-compose info columns
+    info_new=""
+    m=0
+    for key,value in info_dict.items():
+        if "&no&id" in key and m==0 and key!="CSQ":
+            info_new=value
+            m=1
+        elif "&no&id" in key and m==1 and key!="CSQ":
+            info_new=info_new+";"+value
+            m=1
+        elif "&no&id" not in key and m==0 and key!="CSQ":
+            info_new=str(key)+"="+str(value) 
+            m=1
+        elif "&no&id" not in key and m==1 and key!="CSQ":
+            info_new=info_new+";"+str(key)+"="+str(value) 
+            m=1   
+#    print(info_new)
+    if ';CSQ=' in item[7] and (id_mode!='sf' or  id_mode=='sc'): 
+        item[7]=info_new+';CSQ='+info_dict['CSQ']
+    else:
+        item[7]=info_new
+    
+    # print out line
+    line='\t'.join(str(w) for w in item)+'\n'        
+    return line
   
 for line in in_vcf:
-    item=line.strip().split('\t')    
-    if line.startswith('##FORMAT=<ID=PR'):
-        trs_out_vcf.write('##FORMAT=<ID=PR,Number=.,Type=Integer,Description="Spanning paired-read support for the ref and alt alleles in the order listed">\n##FORMAT=<ID=SU,Number=.,Type=Integer,Description="Number of pieces of evidence supporting the variant across all samples">\n##FORMAT=<ID=PE,Number=.,Type=Integer,Description="Number of paired-end reads supporting the variant across all samples">\n') 
-        nontrs_out_vcf.write('##FORMAT=<ID=PR,Number=.,Type=Integer,Description="Spanning paired-read support for the ref and alt alleles in the order listed">\n##FORMAT=<ID=SU,Number=.,Type=Integer,Description="Number of pieces of evidence supporting the variant across all samples">\n##FORMAT=<ID=PE,Number=.,Type=Integer,Description="Number of paired-end reads supporting the variant across all samples">\n')         
-    elif line.startswith('#'):
-        trs_out_vcf.write(line)    
-        nontrs_out_vcf.write(line)          
-    elif 'SVTYPE=BND' in item[7] or 'MantaBND' in item[7]:
-        trs_out_vcf.write(vcf_bnd_tf(line))        
+    item=line.strip().split('\t')
+    n=0
+    if line.startswith('#'):
+        out_vcf.write(line)        
     else:
-        nontrs_out_vcf.write(vcf_nonbnd_tf(line))     
+        out_vcf.write(sv_id_tf(line))   
+ 
