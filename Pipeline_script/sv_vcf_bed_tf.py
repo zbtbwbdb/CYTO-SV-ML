8eb69f6768fe8929d4da6519af0513e9d74b13fc
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2022-12-21 00:19:11 -0600

    Update sv_vcf_bed_tf.py

--- Pipeline_script/sv_vcf_bed_tf.py
+++ Pipeline_script/sv_vcf_bed_tf.py
@@ -3,7 +3,7 @@
 # ---example-----------------------------------------------------------------
 # sv_chr  sv_start_bp  sv_end_bp  sv_chr2  sv_type  sv_id
 # chr1    100000       1000000    chr21    BND      chr1:100000:1000000:chr21:BND:MantaBND***** 
-# ---------------------------------------------------------------------------
+# ------------------------------------------------------------------a---------
 ################################################################################################################
 
 import sys,os,re
@@ -16,12 +16,19 @@ def vcf_bed_tf(line):
     info_list=item[7]
     info=info_list.split(';') 
     sv_chr2=item[0]
+    sv_bp_end=item[1]
+    if 'DEL|DUP|INV|INS|TRA|BND' in item[2]:
+        sv_type=re.findall('DEL|DUP|INV|INS|TRA|BND',item[2])[0]
     for inf in info:
-        if inf.startswith('END'):
-          sv_bp_end=inf.split('=')[1]
-        if inf.startswith('CHR2'):
-          sv_chr2=inf.split('=')[1]           
-    line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+str(sv_chr2)+'\tBND\t'+ str(item[2])+'\n'
+        if inf.startswith('END='):
+            sv_bp_end=inf.split('=')[1]
+        if inf.startswith('CHR2='):
+            sv_chr2=inf.split('=')[1]    
+        if inf.startswith('SVTYPE='):
+            sv_type=inf.split('=')[1] 
+    if 'chr' not in item[2]:       
+        item[2]=str(item[0])+':'+str(item[1])+':'+str(sv_bp_end)+':'+str(item[0])+':'+str(sv_type)+':'+str(item[2])  
+    line=str(item[0])+'\t'+str(item[1])+'\t'+str(sv_bp_end)+'\t'+str(sv_chr2)+'\t'+sv_type+'\t'+ str(item[2])+'\n'
     return line  
   
 for line in in_vcf:
