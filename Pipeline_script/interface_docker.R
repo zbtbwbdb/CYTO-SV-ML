90a22360f060ce85a572668e041b71c683366a1b
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-09 14:34:18 -0600

    Create interface_docker.R

---
+++ Pipeline_script/interface_docker.R
@@ -0,0 +1,16 @@
+library(optparse)
+library(cluster)
+library(reghelper)
+library(bios2mds)
+library(Hmisc)
+library(stringr)
+library(GOSemSim)
+library(org.Hs.eg.db)
+library('biomaRt')
+################################################################################################################################################################################
+
+option_list<-list(make_option(c("-i", "--input_file"),type="character",help="input file", default=NA,metavar="filename"), make_option(c("-o", "--output_file"),type="character",help="output file", default=NA,metavar="filename"))
+opt_parser<-OptionParser(option_list=option_list)
+opt=parse_args(opt_parser)
+input<-as.character(opt$input_file)
+opc<-as.character(opt$output_file)
