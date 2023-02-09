48ef894dbf5035645afdb453f49ba951531f0b74
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-09 15:07:57 -0600

    Update interface_docker.sh

--- Pipeline_script/interface_docker.sh
+++ Pipeline_script/interface_docker.sh
@@ -5,7 +5,8 @@ sample_all=$3
 
 echo ${main_dir} ${cyto_sv_ml_dir} ${sample_all}
 echo "# build R-shiny based user interface in docker container " && date   
-python ${cyto_sv_ml_dir}/Pipeline_script/interface_docker.py -t ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_trs_'${k}'_trs_EXP' -n ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_nontrs_'${k}'_nontrs_EXP' -i ${main_dir}'/out/'${sample_all}'.sv.all.combine_all'
+python ${cyto_sv_ml_dir}/Pipeline_script/interface_data.py -t ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_trs_'${k}'_trs_EXP' -n ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_nontrs_'${k}'_nontrs_EXP' -i ${main_dir}'/out/'${sample_all}'.sv.all.combine_all'
+Rscript ${cyto_sv_ml_dir}/Pipeline_script/interface_data.R 
 cd ${cyto_sv_ml_dir}/cyto-sv-ml/
 sudo docker build -t cyto-sv-ml-app:${sample_all} .
 # to run the docker in the local machine and open user interface with "http://localhost:8000/"
