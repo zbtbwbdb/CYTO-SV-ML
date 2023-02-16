a8eabdee93a6b02baff043d25fb813ec1fc80509
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-15 18:10:40 -0600

    Update interface_data.py

--- Pipeline_script/interface_data.py
+++ Pipeline_script/interface_data.py
@@ -40,16 +40,18 @@ if inFile == "":
 	sys.exit()
       
 # load the optimal model
-automl_trs = AutoML(mode="Explain", n_jobs= 6, results_path=trs_dir+'_EXP')
-automl_nontrs = AutoML(mode="Explain", n_jobs= 6, results_path=nontrs_dir+'_EXP')
+automl_trs = AutoML(mode="Explain", n_jobs= 6, results_path=trs_dir+'_ts_EXP')
+automl_nontrs = AutoML(mode="Explain", n_jobs= 6, results_path=nontrs_dir+'_ts_EXP')
 
 # load the training data tranformer
-model_trs=pickle.load(open(trs_dir+'.pickle', 'rb'))
-model_nontrs=pickle.load(open(nontrs_dir+'.pickle', 'rb')) 
+model_trs=pickle.load(open(trs_dir+'_tf.pickle', 'rb'))
+model_nontrs=pickle.load(open(nontrs_dir+'_tf.pickle', 'rb')) 
 
 # read in all WGS SV data for model prediction
 X_trs=pd.read_csv(inFile+'.sv.all.combine_all_trs',sep='\t',header=0, index_col=None, keep_default_na=False)
 X_nontrs=pd.read_csv(inFile+'.sv.all.combine_all_nontrs',sep='\t',header=0, index_col=None, keep_default_na=False)
+X_trs_all=pd.read_csv(inFile+'.sv.all.combine_all_trs_all',sep='\t',header=0, index_col=None, keep_default_na=False)
+X_nontrs_all=pd.read_csv(inFile+'.sv.all.combine_all_nontrs_all',sep='\t',header=0, index_col=None, keep_default_na=False)
 X_all=pd.read_csv(inFile+'.sv.all.combine_all',sep='\t',header=0, index_col=None, keep_default_na=False)
 
 # transform all the original SV data
@@ -61,20 +63,28 @@ trs_label=automl_trs.predict_all(s_trs)
 nontrs_label=automl_nontrs.predict_all(s_nontrs)
 
 # attach the prediction label to all the original SV data
-X_trs['predict_label','prediction_TA','prediction_TG','prediction_TS']=trs_label
-X_nontrs['predict_label','prediction_TA','prediction_TG','prediction_TS']=nontrs_label
-tmp_id=['sv_type','sv_bp_st_cc1','sv_bp_end_cc1','sv_bp_st_cc_v1','sv_bp_end_cc_v1','sv_bp_st_cc_v2','sv_bp_end_cc_v2','cipos_range','ciend_range','PR_read_ratio','SR_read_ratio']
-X_trs['tmp_id']=X_trs.loc[:,tmp_id].apply(lambda x: ':'.join(str(w) for w in x), axis=1)      
-X_nontrs['tmp_id']=X_nontrs.loc[:,tmp_id].apply(lambda x: ':'.join(str(w) for w in x), axis=1) 
-X_tmp=pd.concat([X_trs['tmp_id','predict_label','prediction_TA','prediction_TG','prediction_TS'],X_nontrs['tmp_id','predict_label','prediction_TA','prediction_TG','prediction_TS']])
+X_trs_all.loc[:,['predict_label','prediction_TA','prediction_TG','prediction_TS']]=trs_label
+X_nontrs_all.loc[:,['predict_label','prediction_TA','prediction_TG','prediction_TS']]=nontrs_label
+tmp_id=['sv_id','sample_id']
+X_trs_all['tmp_id']=0
+X_nontrs_all['tmp_id']=0
+X_trs_all.loc[:,'tmp_id']=X_trs_all.loc[:,tmp_id].apply(lambda x: ':'.join(str(w) for w in x), axis=1)      
+X_nontrs_all.loc[:,'tmp_id']=X_nontrs_all.loc[:,tmp_id].apply(lambda x: ':'.join(str(w) for w in x), axis=1) 
+print(X_trs_all.iloc[0:5,:])
+print(X_nontrs_all.iloc[0:5,:])
+X_tmp=pd.concat([X_trs_all[['tmp_id','predict_label','prediction_TA','prediction_TG','prediction_TS']],X_nontrs_all[['tmp_id','predict_label','prediction_TA','prediction_TG','prediction_TS']]])
+print(X_tmp.iloc[0:5,:])
+X_all['tmp_id']=0
 X_all['tmp_id']=X_all.loc[:,tmp_id].apply(lambda x: ':'.join(str(w) for w in x), axis=1) 
-X_all['predict_label','prediction_TA','prediction_TG','prediction_TS']=X_tmp['predict_label','prediction_TA','prediction_TG','prediction_TS']
-
+X_all.loc[:,['predict_label','prediction_TA','prediction_TG','prediction_TS']]='NA'
+X_all=pd.merge(X_all,X_tmp,how='left',on='tmp_id')
+print(X_all.columns)
 # re-organize the data
-X_all=X_all.rename(columns={'old_col':'new_col','old_col':'new_col'})
-col_drop=[str(w) for w in X_trs.columns if w not in ['sv_type', 'sv_chr', 'sv_chr2', 'sv_read_r', 'sv_read_a', 'sv_read_ratio', 'sv_read_diff', 'sv_bp_st', 'sv_bp_end', 'sv_bp_st_ci0', 'sv_bp_st_ci1', 'sv_bp_end_ci0', 'sv_bp_end_ci1', 'sv_bp_st_ci_range', 'sv_bp_end_ci_range', 'sv_bp_st_cc_v1', 'sv_bp_end_cc_v1', 'sv_database', 'predict_label', 'prediction_TA', 'prediction_TG', 'prediction_TS', 'label', 'predict_max']]
+#X_all=X_all.rename(columns={'old_col':'new_col','old_col':'new_col'})
+col_drop=[str(w) for w in X_all.columns if w not in ['sv_type', 'sv_chr', 'sv_chr2', 'sv_read_r', 'sv_read_a', 'sv_read_ratio', 'sv_read_diff', 'sv_bp_st', 'sv_bp_end', 'sv_bp_st_ci0', 'sv_bp_st_ci1', 'sv_bp_end_ci0', 'sv_bp_end_ci1', 'sv_bp_st_ci_range', 'sv_bp_end_ci_range', 'sv_bp_st_cc_v1', 'sv_bp_end_cc_v1', 'sv_database', 'predict_label', 'prediction_TA', 'prediction_TG', 'prediction_TS', 'label', 'predict_max']]
 X_all=X_all.drop(col_drop,axis=1)
 
 # save all the SV data
-pd.to_csv(X_trs,inFile+'trs',sep='\t',index=False,header=True)
-pd.to_csv(X_nontrs,inFile+'nontrs',sep='\t',index=False,header=True)
+X_trs.to_csv(inFile+'trs',sep='\t',index=False,header=True)
+X_nontrs.to_csv(inFile+'nontrs',sep='\t',index=False,header=True)
+X_all.to_csv(inFile+'nontrs',sep='\t',index=False,header=True)
