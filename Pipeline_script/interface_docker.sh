f0341fdb3d0be8e1a94e1c7aac6fc9a6d993d234
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-16 15:25:19 -0600

    Update interface_docker.sh

--- Pipeline_script/interface_docker.sh
+++ Pipeline_script/interface_docker.sh
@@ -2,13 +2,13 @@
 main_dir=$1
 cyto_sv_ml_dir=$2
 sample_all=$3
-cyto_dict_file=$4
+cyto_band_file=$4
 k=5
 
 echo ${main_dir} ${cyto_sv_ml_dir} ${sample_all}
 echo "# build R-shiny based user interface in docker container " && date   
 python ${cyto_sv_ml_dir}/Pipeline_script/interface_data.py -t ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_trs_'${k}'_ts_EXP' -n ${main_dir}'/out/cyto_sv_ml/'${sample_all}'_nontrs_'${k}'_ts_EXP' -i ${main_dir}'/out/'${sample_all}
-Rscript ${cyto_sv_ml_dir}/Pipeline_script/interface_data.R -i ${main_dir}'/out/'${sample_all} -o ${cyto_sv_ml_dir}/cyto-sv-ml/data/cyto_sv_ml.RData -c ${cyto_dict_file}
+Rscript ${cyto_sv_ml_dir}/Pipeline_script/interface_data.R -i ${main_dir}'/out/'${sample_all} -o ${cyto_sv_ml_dir}/cyto-sv-ml/data/cyto_sv_ml.RData -c ${cyto_band_file}
 cd ${cyto_sv_ml_dir}/
 sudo docker build -t cyto-sv-ml-app:${sample_all} .
 # to run the docker image in the local machine and open user interface with "http://localhost:8000/"
