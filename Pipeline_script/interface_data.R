96357a19502decca8dddc30734975047d47226bd
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-16 13:45:06 -0600

    Update interface_data.R

--- Pipeline_script/interface_data.R
+++ Pipeline_script/interface_data.R
@@ -13,11 +13,11 @@ option_list<-list(make_option(c("-i", "--input_file"),type="character",help="inp
 opt_parser<-OptionParser(option_list=option_list)
 opt=parse_args(opt_parser)
 input<-as.character(opt$input_file)
-opc<-as.character(opt$output_file)
+output<-as.character(opt$output_file)
 
 # read in trs and nontrs data
-X_all=read.table(paste(inFile,'nontrs',sep=''),sep='\t',index=False,header=True)
+X_all=read.table(paste(inFile,'.all_pred',sep=''),sep='\t',index=False,header=True)
 cyto_band_dict=read.table(cyto_dict_file,sep='\t',index=False,header=True)
 
 # save the SV data
-save(X_all,cyto_band_dict, file = paste(inFile+'_all',sep=''))
+save(X_all,cyto_band_dict, file = output)
