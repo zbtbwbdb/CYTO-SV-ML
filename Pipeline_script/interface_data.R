135e1cec29a6dfa20952e23e7882c4a46f0244de
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-16 15:26:21 -0600

    Update interface_data.R

--- Pipeline_script/interface_data.R
+++ Pipeline_script/interface_data.R
@@ -9,16 +9,16 @@ library(org.Hs.eg.db)
 library('biomaRt')
 ################################################################################################################################################################################
 
-option_list<-list(make_option(c("-i", "--input_file"),type="character",help="input file", default=NA,metavar="filename"), make_option(c("-o", "--output_file"),type="character",help="output file", default=NA,metavar="filename"), make_option(c("-c", "--cyto_dict_file"),type="character",help="cyto_dict file", default=NA,metavar="filename"))
+option_list<-list(make_option(c("-i", "--input_file"),type="character",help="input file", default=NA,metavar="filename"), make_option(c("-o", "--output_file"),type="character",help="output file", default=NA,metavar="filename"), make_option(c("-c", "--cyto_band_file"),type="character",help="cyto band file", default=NA,metavar="filename"))
 opt_parser<-OptionParser(option_list=option_list)
 opt=parse_args(opt_parser)
 input<-as.character(opt$input_file)
 output<-as.character(opt$output_file)
-cyto_dict_file<-as.character(opt$cyto_dict_file)
+cyto_band_file<-as.character(opt$cyto_band_file)
 
 # read in trs and nontrs data
 X_all=read.table(paste(inFile,'.all_pred',sep=''),sep='\t',index=False,header=True)
-cyto_band_dict=read.table(cyto_dict_file,sep='\t',index=False,header=True)
+cyto_band_dict=read.table(cyto_band_file,sep='\t',index=False,header=True)
 
 # save the SV data
 save(X_all,cyto_band_dict, file = output)
