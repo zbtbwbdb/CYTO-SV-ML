295a05db371c124bfbb472efa1e0e4779e476c32
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-16 15:51:27 -0600

    Update interface_data.R

--- Pipeline_script/interface_data.R
+++ Pipeline_script/interface_data.R
@@ -1,12 +1,5 @@
 library(optparse)
-library(cluster)
-library(reghelper)
-library(bios2mds)
-library(Hmisc)
-library(stringr)
-library(GOSemSim)
-library(org.Hs.eg.db)
-library('biomaRt')
+
 ################################################################################################################################################################################
 
 option_list<-list(make_option(c("-i", "--input_file"),type="character",help="input file", default=NA,metavar="filename"), make_option(c("-o", "--output_file"),type="character",help="output file", default=NA,metavar="filename"), make_option(c("-c", "--cyto_band_file"),type="character",help="cyto band file", default=NA,metavar="filename"))
@@ -17,8 +10,8 @@ output<-as.character(opt$output_file)
 cyto_band_file<-as.character(opt$cyto_band_file)
 
 # read in trs and nontrs data
-X_all=read.table(paste(inFile,'.all_pred',sep=''),sep='\t',index=False,header=True)
-cyto_band_dict=read.table(cyto_band_file,sep='\t',index=False,header=True)
+X_all=read.table(paste(input,'.all_pred',sep=''),sep='\t',header=T)
+cyto_band_dict=read.table(cyto_band_file,sep='\t',header=T)
 
 # save the SV data
 save(X_all,cyto_band_dict, file = output)
