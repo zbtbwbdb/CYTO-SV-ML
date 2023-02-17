a304066ffd5a6c964b083a88d0541e50535b57aa
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-02-17 09:12:44 -0600

    Update interface_data.R

--- Pipeline_script/interface_data.R
+++ Pipeline_script/interface_data.R
@@ -13,5 +13,14 @@ cyto_band_file<-as.character(opt$cyto_band_file)
 all_sv=read.table(paste(input,'.all_pred',sep=''),sep='\t',header=T)
 genome_coor=read.table(cyto_band_file,sep='\t',header=T)
 
+# recode the label
+all_sv[all_sv['label']=='0', 'label']='UC'
+all_sv[all_sv['label']=='-1', 'label']='TA'
+all_sv[all_sv['label']=='1', 'label']='TG'
+all_sv[all_sv['label']=='2', 'label']='TS'
+all_sv[all_sv['predict_label']=='-1','predict_label']='TA'
+all_sv[all_sv['predict_label']=='1','predict_label']='TG'
+all_sv[all_sv['predict_label']=='2','predict_label']='TS'
+
 # save the SV data
 save(all_sv,genome_coor, file = output)
