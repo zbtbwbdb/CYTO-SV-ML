41bfef8a285999b20100f42dbe434ffa281d8a57
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-04-25 10:35:18 -0500

    Update sv_bnd_database_mapping.py

--- Pipeline_script/sv_bnd_database_mapping.py
+++ Pipeline_script/sv_bnd_database_mapping.py
@@ -17,7 +17,7 @@ import sys,os,re
 import pandas as pd
 
 # read the sv input data and database
-ref_data=pd.read_csv(sys.argv[1],sep='\t',header=0)
+ref_data=pd.read_csv(sys.argv[1],sep='\t',header=None)
 in_vcf=pd.read_csv(sys.argv[2],sep='\t',header=0,keep_default_na=False)
 out_vcf=str(sys.argv[2])+"."+str(sys.argv[3])
 bp_dis=int(str(sys.argv[3]).split('_')[2])
@@ -30,16 +30,18 @@ for i in range(in_vcf.shape[0]):
     bp1=int(in_vcf.iloc[i,1])
     chr2=str(in_vcf.iloc[i,3])   
     bp2=int(in_vcf.iloc[i,2])
-    
     # extract mapping records 
     bnd_dict=ref_data[(ref_data.iloc[:,0]==chr1) & (ref_data.iloc[:,3]==chr2) & (abs(ref_data.iloc[:,1].astype(int)-bp1)<=bp_dis) & (abs(ref_data.iloc[:,2].astype(int)-bp2)<=bp_dis)] 
     if bnd_dict.empty or bnd_dict.shape[0]==0:
         continue
     else:
-        bnd_dict=bnd_dict.astype(str)
-        bnd_dict['info']=bnd_dict.apply(lambda x: '|'.join(x.tolist()), axis=1)      
-        in_vcf.iloc[i,in_vcf.shape[1]-1]=bnd_dict['info'].str.cat(sep='&')#re.sub('\t| ',',',str(list(bnd_dict)))
-#        print(in_vcf.iloc[i,:])
+#         print(in_vcf.iloc[i,:])        
+#         print(bnd_dict)
+        bnd_dict.iloc[:,1]=abs(bnd_dict.iloc[:,1].astype(int)-bp1)
+        bnd_dict.iloc[:,2]=abs(bnd_dict.iloc[:,2].astype(int)-bp2) 
+        bnd_dict['info']=bnd_dict.apply(lambda x: max(x[1:3]),axis=1)
+#         print(bnd_dict['info'])
+        in_vcf.iloc[i,in_vcf.shape[1]-1]=min(bnd_dict['info']) 
 
 # export to csv
 in_vcf.to_csv(out_vcf,sep='\t',index=False,header=False)
