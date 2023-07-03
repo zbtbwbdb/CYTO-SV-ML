6ddb175aaba66bf9a55644b9cfb83a718800b387
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-07-02 21:38:07 -0500

    Update sv_vcf_tf.sh

--- Pipeline_script/sv_vcf_tf.sh
+++ Pipeline_script/sv_vcf_tf.sh
@@ -13,7 +13,7 @@ for sv_caller in  breakdancer cnvnator delly.deletion delly.duplication delly.in
         echo ${sv_caller}
         ls ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.list
         SURVIVOR merge ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.list 1000 0 1 0 0 10  ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s
-        python ${cyto_sv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s sc
+        python ${cyto_sv_ml_dir}/Pipeline_script/sv_id_tf.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s sf
         mv ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s.re_id ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s    
         echo ${sv_caller} "SV size tf" && date     
         python ${cyto_sv_ml_dir}/Pipeline_script/sv_size.py ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.s $size down > ${main_dir}/out/${sample}/sv_caller_results/${sample}.${sv_caller}.vcf.${size_k}k
