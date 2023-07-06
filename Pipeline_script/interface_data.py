1867a9f349ad5739efa8650302d69d340dcefa3e
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-07-06 12:29:45 -0500

    Update interface_data.py

--- Pipeline_script/interface_data.py
+++ Pipeline_script/interface_data.py
@@ -116,7 +116,7 @@ X_trs_all.to_csv(inFile+'.trs_pred',sep='\t',index=False,header=True)
 X_nontrs_all.to_csv(inFile+'.nontrs_pred',sep='\t',index=False,header=True)
 
 # re-organize trs/nontrs data
-col_keep=['sv_type', 'sv_chr', 'sv_chr2', 'sv_read_r', 'sv_read_a','sv_PR_ref','sv_SR_ref','sv_PR_alt','sv_SR_alt', 'read_ratio', 'read_ratio_diff', 'sv_bp_st', 'sv_bp_end', 'ci_pos0', 'ci_pos1', 'sv_bp_end_POS', 'sv_bp_end_END', 'cipos_range', 'ciend_range', 'sv_bp_st_cc_v1', 'sv_bp_end_cc_v1', 'sv_database', 'predict_label', 'prediction_TA', 'prediction_TG', 'prediction_TS', 'label'] 
+col_keep=['sv_type', 'sv_chr1', 'sv_chr2', 'sv_read_r', 'sv_read_a','sv_PR_ref','sv_SR_ref','sv_PR_alt','sv_SR_alt', 'read_ratio', 'read_ratio_diff', 'sv_bp_st', 'sv_bp_end', 'ci_pos0', 'ci_pos1', 'sv_bp_end_POS', 'sv_bp_end_END', 'cipos_range', 'ciend_range', 'sv_bp_st_cc_v1', 'sv_bp_end_cc_v1', 'sv_database', 'predict_label', 'prediction_TA', 'prediction_TG', 'prediction_TS', 'label'] 
 col_drop=[str(w) for w in X_trs_all.columns if w not in col_keep]
 print(col_drop)
 X_trs_all=X_trs_all.drop(col_drop,axis=1)
@@ -128,6 +128,7 @@ print(X_nontrs_all.columns)
 
 # combined trs/nontrs data
 X_all=pd.concat([X_trs_all,X_nontrs_all],axis=0)
+X_all=X_all.rename(columns={'QUAL':'svtyper_score','SUPP':'sv_caller_supp'})  
 
 # attach the prediction label to all the original SV data
 X_all['predict_max']=0
