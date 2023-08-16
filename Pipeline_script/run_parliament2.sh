1df0c80b3bebea3900f25c144dc59dfbc8bf3bff
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-08-16 11:52:15 -0500

    Update run_parliament2.sh

--- Pipeline_script/run_parliament2.sh
+++ Pipeline_script/run_parliament2.sh
@@ -13,4 +13,4 @@ cp ${cyto_sv_ml_dir}/reference/hg38/hs38.fasta ${main_dir}/in/reference/hg38/hs3
 cp ${cyto_sv_ml_dir}/reference/hg38/hs38.fasta.fai ${main_dir}/in/reference/hg38/hs38.fasta.fai
 sudo docker run --rm --privileged -v ${main_dir}/in/:/home/dnanexus/in -v ${main_dir}/out/${sample}/:/home/dnanexus/out ${parliament_docker} --bam ${sample}.bam  --bai ${sample}.bam.bai -r reference/hg38/hs38.fasta --fai reference/hg38/hs38.fasta.fai --prefix ${sample} --filter_short_contigs --breakdancer --cnvnator --delly_deletion --delly_inversion --delly_duplication
 #rm -rf ${main_dir}/in/input.bam*
-rm -rf ${main_dir}/in/reference/
+sudo rm -rf ${main_dir}/in/reference/
