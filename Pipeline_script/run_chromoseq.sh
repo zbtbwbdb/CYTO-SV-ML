51435f470ee375c519ac90c98179564380e56f7a
tzhang-nmdp <56171080+tzhang-nmdp@users.noreply.github.com>
2023-08-17 12:58:48 -0500

    Update run_chromoseq.sh

--- Pipeline_script/run_chromoseq.sh
+++ Pipeline_script/run_chromoseq.sh
@@ -20,7 +20,7 @@ echo "# prepare the SV db uwstl_s file for chromoseq pipeline" && date
 awk '($1=="DUP")||($1=="DEL")||($1=="INS")||($1=="INV"){print $2"\t"$3"\t"$5"\t"$1"\t"$2":"$3":"$5":"$1}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.chromoseq.txt > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs
 sort -k 1,1 -k 2,3n ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs | bgzip -f > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs.gz
 tabix -p vcf ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.nontrs.gz
-awk '($1=="BND"){print $2"\t"$3"\t"$5"\t"$4"\t"$2":"$3":"$5":$4":"$1}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.chromoseq.txt > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.trs   
+awk '($1=="BND"){print $2"\t"$3"\t"$5"\t"$4"\t"$2":"$3":"$5":"$4":"$1}' ${main_dir}/out/${sample}/sv_caller_results/${sample}.chromoseq.txt > ${main_dir}/out/${sample}/sv_caller_results/${sample}.uwstl_s.trs   
          
 echo "# prepare the SV vcf files (manta + ichnorcnv)" && date 
 gunzip -f ${main_dir}/out/${sample}/sv_caller_results/${sample}.svs_annotated.vcf.gz 
